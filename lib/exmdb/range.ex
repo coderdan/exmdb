417c149d30aceaff8201a823f9c137e2f7852236
Dan Draper <dan@coderdan.co>
2019-08-25 19:21:36 +1000

    Removed debugging

--- lib/exmdb/range.ex
+++ lib/exmdb/range.ex
@@ -73,9 +73,7 @@ defmodule Exmdb.Range do
     {:ok, range}
   end
 
-  def cursor_open(%Range{src: %Txn{res: txn_res, type: txn_type}, db_spec: db_spec} = r) do
-    IO.inspect(r, label: "EXMDB cursor_open")
-
+  def cursor_open(%Range{src: %Txn{res: txn_res, type: txn_type}, db_spec: db_spec}) do
     {dbi, _key_type, _val_type} = db_spec
     case txn_type do
       :ro -> :elmdb.ro_txn_cursor_open(txn_res, dbi)
